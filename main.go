1163aeb558959802a4287cab9a7633ca1303d383
vandosant <sjskender@gmail.com>
2014-11-09 11:38:46 -0700

    Only count arguments

--- main.go
+++ main.go
@@ -11,12 +11,12 @@ func init() {
 
 func main() {
 	// Program Name is the first argument
-	cmd := os.Args[0]
+	cmd := len(os.Args[1:])
 	fmt.Printf("Program Name: %s\n", cmd)
 
 	// Count the number of arguments
 	argCount := len(os.Args[1:])
-	fmt.Printf("Total Arguments (excluding program name): %d\n", argCount)
+	fmt.Printf("Total Arguments: %d\n", argCount)
 
 	for i, a := range os.Args[1:] {
 		fmt.Printf("Argument %d is %s\n", i+1, a)
