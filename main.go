0909cfa821cecf758cb227a88ab1184f9b94a4c8
vandosant <sjskender@gmail.com>
2014-11-09 11:24:26 -0700

    Count the number of argument names

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 func main() {
 	// Program Name is the first argument
 	cmd := os.Args[0]
-
 	fmt.Printf("Program Name: %s\n", cmd)
+
+	// Count the number of arguments
+	argCount := len(os.Args[1:])
+	fmt.Printf("Total Arguments (excluding program name): %d\n", argCount)
 }
